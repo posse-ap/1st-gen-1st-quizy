69768e3659db485b0123ca63ed8a3cfbc074d488
yuka-hironaka <hrnk.yuka@gmail.com>
2020-11-16 01:26:13 +0900

    1st-quizy

---
+++ kuizy.js
@@ -0,0 +1,14 @@
+'use strict'
+const '1-1'=document.getElementById("1-1");
+
+1-1.addEventListener('1-1', () => {
+    1-1.style.backgroundColor = "lightblue";
+  });
+
+  1-1.addEventListener('mouseover', () => {
+    button1.style.backgroundColor = "orange";
+  });
+
+const answers=[
+    '正解!',
+];
